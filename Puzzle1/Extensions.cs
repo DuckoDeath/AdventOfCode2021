d502ad49afe4a2c41cf64d95a5bbcd5dcc9ea2f1
Scott <scott@noneofyourbusiness.com>
2021-12-01 08:33:47 -0700

    Add project files.

---
+++ Puzzle1/Extensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Puzzle1
+{
+    public static class Extensions
+    {
+        public static IEnumerable<TResult> SelectWithPrevious<TSource, TResult>
+        (this IEnumerable<TSource> source,
+         Func<TSource, TSource, TResult> projection)
+        {
+            using (var iterator = source.GetEnumerator())
+            {
+                if (!iterator.MoveNext())
+                {
+                    yield break;
+                }
+                TSource previous = iterator.Current;
+                while (iterator.MoveNext())
+                {
+                    yield return projection(previous, iterator.Current);
+                    previous = iterator.Current;
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> SelectWithPreviousQuad<TSource, TResult>
+        (this IEnumerable<TSource> source,
+         Func<TSource, TSource, TSource, TSource, TResult> projection)
+        {
+            using (var iterator = source.GetEnumerator())
+            {
+                if (!iterator.MoveNext())
+                {
+                    yield break;
+                }
+                TSource one = iterator.Current;
+                if (!iterator.MoveNext())
+                {
+                    yield break;
+                }
+                TSource two = iterator.Current;
+                if (!iterator.MoveNext())
+                {
+                    yield break;
+                }
+                TSource three = iterator.Current;
+                while (iterator.MoveNext())
+                {
+                    yield return projection(one, two, three, iterator.Current);
+                    one = two;
+                    two = three;
+                    three = iterator.Current;
+                }
+            }
+        }
+    }
+}
---
+++ Puzzle1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Puzzle1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(@"..\..\input.txt").Select(x=>int.Parse(x));
+            Console.WriteLine("Lines: " + lines.Count());
+
+            var count = lines.SelectWithPrevious((prev, curr) => {
+                return curr > prev ? 1 : 0;
+            }).Sum();
+            Console.WriteLine("Part One");
+            Console.WriteLine("Answer:" + count);
+
+            count = lines.SelectWithPreviousQuad((one, two, three, four) => {
+                return two + three + four > one + two + three ? 1 : 0;
+            }).Sum();
+            Console.WriteLine("Part Two");
+            Console.WriteLine("Answer:" + count);
+
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+        }
+    }
+}
