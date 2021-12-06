20b27b48e76e580d32b239ed3c9442467cc76afa
Scott <scott@noneofyourbusiness.com>
2021-12-05 22:34:43 -0700

    day 6

---
+++ Puzzle6/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime;
+
+namespace Puzzle6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Puzzle 6");
+            Console.WriteLine();
+            ExecutePart1(@"..\..\input.txt");
+            ExecutePart2(@"..\..\input.txt");
+
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+        }
+
+        public static long ExecutePart1(string filename, bool debug = false)
+        {
+            GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
+            Console.WriteLine("Part One");
+            var lines = File.ReadAllLines(filename);
+            Console.WriteLine("Lines: " + lines.Count());
+            var fish = lines.First().Split(',').Select(x => int.Parse(x)).ToList();
+            Dictionary<int, long> fishes = CreateFishes();
+            foreach (var f in fish)
+            {
+                fishes[f]++;
+            }
+
+            for (int i=0; i < 80; i++)
+            {
+                Dictionary<int, long> newFishes = new Dictionary<int, long>();
+                foreach (var f in fishes.Keys.OrderByDescending(x=>x))
+                {
+                    var cnt = fishes[f];
+                    if (f==0)
+                    {
+                        newFishes[8] = cnt;
+                        newFishes[6] += cnt;
+                    } else
+                    {
+                        newFishes[f-1] = cnt;
+                    }
+                }
+                fishes = newFishes;
+            }
+
+            long result = 0;
+            foreach (var f in fishes)
+            {
+                result += f.Value;
+            }
+            Console.WriteLine("Answer: " + result);
+            Console.WriteLine();
+            return result;
+        }
+
+        public static long ExecutePart2(string filename, bool debug = false)
+        {
+            GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
+            Console.WriteLine("Part Two");
+            var lines = File.ReadAllLines(filename);
+            Console.WriteLine("Lines: " + lines.Count());
+            var fish = lines.First().Split(',').Select(x => int.Parse(x)).ToList();
+            Dictionary<int, long> fishes = CreateFishes();
+            foreach (var f in fish)
+            {
+                fishes[f]++;
+            }
+
+            for (int i = 0; i < 256; i++)
+            {
+                Dictionary<int, long> newFishes = new Dictionary<int, long>();
+                foreach (var f in fishes.Keys.OrderByDescending(x => x))
+                {
+                    var cnt = fishes[f];
+                    if (f == 0)
+                    {
+                        newFishes[8] = cnt;
+                        newFishes[6] += cnt;
+                    }
+                    else
+                    {
+                        newFishes[f - 1] = cnt;
+                    }
+                }
+                fishes = newFishes;
+            }
+
+            long result = 0;
+            foreach (var f in fishes)
+            {
+                result += f.Value;
+            }
+            Console.WriteLine("Answer: " + result);
+            Console.WriteLine();
+            return result;
+        }
+
+        private static Dictionary<int,long> CreateFishes()
+        {
+            Dictionary<int, long> fishes = new Dictionary<int, long>();
+            for (int i = 0; i <= 8; i++)
+            {
+                fishes.Add(i, 0);
+            }
+            return fishes;
+        }
+
+    }
+}
+
---
+++ UnitTestProject/Day6Test.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Puzzle6;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class Day6Test
+    {
+        [TestMethod]
+        public void TestExamplePart1()
+        {
+            var result = Program.ExecutePart1(@"..\..\example6.txt", true);
+            Assert.AreEqual(5934, result);
+        }
+
+        [TestMethod]
+        public void TestExamplePart2()
+        {
+            var result = Program.ExecutePart2(@"..\..\example6.txt", true);
+            Assert.AreEqual(26984457539, result);
+        }
+
+        [TestMethod]
+        public void TestActualPart1()
+        {
+            var result = Program.ExecutePart1(@"..\..\..\Puzzle6\input.txt");
+            Assert.AreEqual(353274, result);
+        }
+
+        [TestMethod]
+        public void TestActualPart2()
+        {
+            var result = Program.ExecutePart2(@"..\..\..\Puzzle6\input.txt");
+            Assert.AreEqual(1609314870967, result);
+        }
+    }
+}
