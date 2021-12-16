c8038080db19e595608a352792de6c93da2b31a5
Scott <scott@noneofyourbusiness.com>
2021-12-15 17:42:30 -0700

    day 15

---
+++ UnitTestProject/Day15Test.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class Day15Test : AbstractTest
+    {
+        private int day = 15;
+
+        [TestMethod]
+        public void TestExamplePart1()
+        {
+            var result = GetMethod(day, 1).Invoke(null, new object[] { $@"..\..\example{day}.txt", false });
+            Assert.AreEqual(40L, result);
+        }
+
+        [TestMethod]
+        public void TestExamplePart2()
+        {
+            var result = GetMethod(day, 2).Invoke(null, new object[] { $@"..\..\example{day}.txt", true });
+            Assert.AreEqual(315L, result);
+        }
+
+        [TestMethod] 
+        public void TestActualPart1()
+        {
+            var result = GetMethod(day, 1).Invoke(null, new object[] { $@"..\..\..\AdventOfCode\Day{day}\input.txt", false });
+            Assert.AreEqual(745L, result);
+        }
+
+        [TestMethod]
+        public void TestActualPart2()
+        {
+            var result = GetMethod(day, 2).Invoke(null, new object[] { $@"..\..\..\AdventOfCode\Day{day}\input.txt", true });
+            Assert.AreEqual(3002L, result);
+        }
+    }
+}
