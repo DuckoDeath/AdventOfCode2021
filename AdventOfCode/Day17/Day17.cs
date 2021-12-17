3e70e6f258570722df244aa47c4f15433b346ec4
Scott <scott@noneofyourbusiness.com>
2021-12-16 22:41:46 -0700

    day 17

--- AdventOfCode/Day17/Day17.cs
+++ AdventOfCode/Day17/Day17.cs
@@ -23,8 +23,87 @@ namespace AdventOfCode.Day17
         {
             var lines = File.ReadAllLines(filename).ToList();
             Console.WriteLine("Lines: " + lines.Count);
+            // target area: x=102..157, y=-146..-90
+            var line = lines.First();
+            line = line.Replace("target area: ", string.Empty);
+            var parts = line.Split(',');
+            var xp = parts[0];
+            var xpParts = xp.Split(new[] { ".." }, StringSplitOptions.None);
+            var targetX1 = int.Parse(xpParts[0].Replace("x=", string.Empty));
+            var targetX2 = int.Parse(xpParts[1]);
+            if (targetX1 > targetX2)
+            {
+                // reverse
+                var temp = targetX2;
+                targetX2 = targetX1;
+                targetX2 = temp;
+            }
+            var yp = parts[1];
+            var ypParts = yp.Split(new[] { ".." }, StringSplitOptions.None);
+            var targetY1 = int.Parse(ypParts[0].Replace("y=", string.Empty));
+            var targetY2 = int.Parse(ypParts[1]);
+            if (targetY1 > targetY2)
+            {
+                // reverse
+                var temp = targetY2;
+                targetY2 = targetY1;
+                targetY2 = temp;
+            }
 
-            var result = 0;
+            var allSuccess = new List<KeyValuePair<int, int>>();
+            var maxY = int.MinValue;
+            List<KeyValuePair<int,int>> maxPath = null;
+
+            for (var startVx = 1; startVx < 999; startVx++)
+            {
+                for (var startVy = -999; startVy < 999; startVy++)
+                {
+                    var vx = startVx;
+                    var vy = startVy;
+                    var path = new List<KeyValuePair<int, int>>();
+                    var done = false;
+                    var success = false;
+
+                    var x = 0;
+                    var y = 0;
+                    path.Add(new KeyValuePair<int, int>(x, y));
+                    while (!done)
+                    {
+                        x = x + vx;
+                        y = y + vy;
+                        if (vx != 0)
+                        {
+                            vx += (vx > 0 ? -1 : 1);
+                        }
+                        vy -= 1;
+                        path.Add(new KeyValuePair<int, int>(x, y));
+                        success = (x >= targetX1 && x <= targetX2 && y >= targetY1 && y <= targetY2);
+                        if (success)
+                        {
+                            done = true;
+                            allSuccess.Add(new KeyValuePair<int, int>(startVx, startVy));
+                        } else
+                        {
+                            // also done if missed
+                            if (x > targetX2 || y < targetY1)
+                            {
+                                done = true;
+                            }
+                        }
+                    }
+                    if (success)
+                    {
+                        var pathMaxY = path.Select(p => p.Value).Max();
+                        if (pathMaxY > maxY)
+                        {
+                            maxPath = path;
+                            maxY = pathMaxY;
+                        }
+                    }
+                }
+            }
+
+            var result = part==1 ? maxY : allSuccess.Count;
             Console.WriteLine("Answer: " + result);
             Console.WriteLine();
             return result;
--- UnitTestProject/Day17Test.cs
+++ UnitTestProject/Day17Test.cs
@@ -11,28 +11,28 @@ namespace UnitTestProject
         public void TestExamplePart1()
         {
             var result = GetMethod(day, 1).Invoke(null, new object[] { $@"..\..\example{day}.txt", false });
-            Assert.AreEqual(40L, result);
+            Assert.AreEqual(45L, result);
         }
 
         [TestMethod]
         public void TestExamplePart2()
         {
             var result = GetMethod(day, 2).Invoke(null, new object[] { $@"..\..\example{day}.txt", true });
-            Assert.AreEqual(315L, result);
+            Assert.AreEqual(112L, result);
         }
 
         [TestMethod]
         public void TestActualPart1()
         {
             var result = GetMethod(day, 1).Invoke(null, new object[] { $@"..\..\..\AdventOfCode\Day{day}\input.txt", false });
-            Assert.AreEqual(745L, result);
+            Assert.AreEqual(10585L, result);
         }
 
         [TestMethod]
         public void TestActualPart2()
         {
             var result = GetMethod(day, 2).Invoke(null, new object[] { $@"..\..\..\AdventOfCode\Day{day}\input.txt", true });
-            Assert.AreEqual(3002L, result);
+            Assert.AreEqual(5247L, result);
         }
     }
 }
