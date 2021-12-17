9d19a1329d91fccd96324c04d82146c1f9e88a70
Scott <scott@noneofyourbusiness.com>
2021-12-16 21:56:27 -0700

    start day 17

---
+++ AdventOfCode/Day17/Day17.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Day17
+{
+    public class Day17
+    {
+        public static long ExecutePart1(string filename, bool debug = false)
+        {
+            Console.WriteLine("Part One");
+            return DoExecutePart(filename, 1, debug);
+        }
+
+        public static long ExecutePart2(string filename, bool debug = false)
+        {
+            Console.WriteLine("Part Two");
+            return DoExecutePart(filename, 2, debug);
+        }
+
+        private static long DoExecutePart(string filename, int part, bool debug = false)
+        {
+            var lines = File.ReadAllLines(filename).ToList();
+            Console.WriteLine("Lines: " + lines.Count);
+
+            var result = 0;
+            Console.WriteLine("Answer: " + result);
+            Console.WriteLine();
+            return result;
+        }
+
+    }
+}
